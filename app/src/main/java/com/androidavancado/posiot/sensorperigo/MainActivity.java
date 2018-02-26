ba95797b41bd4ef715732cbeb972e0e562e6337d
daniellengs <danielle.nathalia@gmail.com>
2018-02-25 22:15:29 -0300

    Activitys
    Actitivys iniciais

--- app/src/main/java/com/androidavancado/posiot/sensorperigo/MainActivity.java
+++ app/src/main/java/com/androidavancado/posiot/sensorperigo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.androidavancado.posiot.sensorperigo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void abrirPerfil(View v) {
+      //  Intent it = new Intent(this, PerfilActivity.class);
+      //  startActivity(it);
+
+    }
+
+    public void abrirMenu(View v) {
+      //  Intent it = new Intent(this, MainMenuActivity.class);
+      //  startActivity(it);
+
+    }
+
 }
