0a692a9ae40aa11d974f29fa3c861ba17673faa6
Erika Pessôa <erika.pessoa@gmail.com>
2018-03-06 14:30:17 -0300

    Constantes Atualizadas de Estado Cívil e Sexo

--- app/src/main/java/com/androidavancado/posiot/sensorperigo/util/Constants.java
+++ app/src/main/java/com/androidavancado/posiot/sensorperigo/util/Constants.java
@@ -5,4 +5,16 @@ package com.androidavancado.posiot.sensorperigo.util;
  */
 
 public class Constants {
+
+    public static final char FEMININ_SEX = 'F';
+    public static final char MASCULIN_SEX = 'M';
+    public static final char OTHER_SEX = 'O';
+
+    public static final String SINGLE_MARITAL_STATUS  = "SINGLE";
+    public static final String WIDOW_MARITAL_STATUS  = "WIDOW";
+    public static final String MARRIED_MARITAL_STATUS  = "MARRIED";
+    public static final String DIVORCED_MARITAL_STATUS  = "DIVORCED";
+
+
+
 }
