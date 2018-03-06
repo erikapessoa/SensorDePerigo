267f9115c0833156c222e2e76662b7e06e757cab
Erika Pessôa <erika.pessoa@gmail.com>
2018-03-06 16:33:19 -0300

    Arquivos modelo do button device e library do knot modificada por Diego

--- app/src/main/java/com/androidavancado/posiot/sensorperigo/model/ButtonData.java
+++ app/src/main/java/com/androidavancado/posiot/sensorperigo/model/ButtonData.java
@@ -1,8 +1,51 @@
 package com.androidavancado.posiot.sensorperigo.model;
 
+import br.org.cesar.knot.lib.model.AbstractThingData;
+
 /**
  * Created by erika on 25/02/18.
  */
 
-public class ButtonData {
+public class ButtonData extends AbstractThingData {
+
+    private String mUuid;
+    private boolean mCurrentValue;
+
+    public boolean getMCurrentValue() {
+        if(data != null){
+
+            Boolean b = (Boolean) data.value;
+            return b;
+        } else {
+            return false;
+        }
+    }
+
+    public String getButtonUUID() {
+        return source;
+    }
+
+    public void setButtonUUID(String buttonUUID) {
+        this.mUuid = buttonUUID;
+    }
+
+    public void setCurrentValue(boolean currentValue) {
+        this.mCurrentValue = currentValue;
+    }
+
+    public void setData(boolean data){
+        super.data.value = data;
+    }
+
+    public boolean getData(){
+        return (Boolean)data.value;
+    }
+
+    @Override
+    public String toString() {
+        String value = super.toString() + " ButtonUUID = " + getButtonUUID() +
+                " currentValue = " + getMCurrentValue();
+        return value;
+    }
+
 }
--- app/src/main/java/com/androidavancado/posiot/sensorperigo/model/ButtonDataMessage.java
+++ app/src/main/java/com/androidavancado/posiot/sensorperigo/model/ButtonDataMessage.java
@@ -1,8 +1,11 @@
 package com.androidavancado.posiot.sensorperigo.model;
 
+import br.org.cesar.knot.lib.model.AbstractThingMessage;
+
 /**
  * Created by erika on 25/02/18.
  */
 
-public class ButtonDataMessage {
+public class ButtonDataMessage extends AbstractThingMessage {
+
 }
