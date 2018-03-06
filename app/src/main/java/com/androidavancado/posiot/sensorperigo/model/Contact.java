eb25df96710fee9c8b2935e77f7dc25466a509eb
Erika Pessôa <erika.pessoa@gmail.com>
2018-03-06 14:25:09 -0300

    Classe de Contato

--- app/src/main/java/com/androidavancado/posiot/sensorperigo/model/Contact.java
+++ app/src/main/java/com/androidavancado/posiot/sensorperigo/model/Contact.java
@@ -5,4 +5,45 @@ package com.androidavancado.posiot.sensorperigo.model;
  */
 
 public class Contact {
+
+    private String mName;
+    private String mEmail;
+    private Phone mCellPhone;
+
+    /**
+     * Nome do contato, e-mail e número do celular.
+     *
+     * @param mName
+     * @param mEmail
+     * @param mCellPhone
+     */
+    public Contact(String mName, String mEmail, Phone mCellPhone) {
+        this.mName = mName;
+        this.mEmail = mEmail;
+        this.mCellPhone = mCellPhone;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmEmail() {
+        return mEmail;
+    }
+
+    public void setmEmail(String mEmail) {
+        this.mEmail = mEmail;
+    }
+
+    public Phone getmCellPhone() {
+        return mCellPhone;
+    }
+
+    public void setmCellPhone(Phone mCellPhone) {
+        this.mCellPhone = mCellPhone;
+    }
 }
